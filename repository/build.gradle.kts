007141e84c2f49caa3b7be52676faa553753b215
Warakorn Mukdaprasert <warakorn.prasert@gmail.com>
2024-06-27 21:08:35 +0700

    add: repository module

---
+++ repository/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+kotlin {
+    kotlin.jvmToolchain(17)
+}
+
+android {
+    namespace = "com.korn.portfolio.randomtrivia.repository"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
