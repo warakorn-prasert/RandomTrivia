e5590217f210701c78f60837b9eb0238b7aa026f
Warakorn Mukdaprasert <warakorn.prasert@gmail.com>
2024-06-29 13:36:35 +0700

    feat: overload getBy in QuestionDao

--- database/src/main/java/com/korn/portfolio/randomtrivia/database/dao/QuestionDao.kt
+++ database/src/main/java/com/korn/portfolio/randomtrivia/database/dao/QuestionDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Query
 import com.korn.portfolio.randomtrivia.database.model.Difficulty
 import com.korn.portfolio.randomtrivia.database.model.entity.Question
 import kotlinx.coroutines.flow.Flow
+import java.util.UUID
 
 @Dao
 interface QuestionDao : BaseDao<Question> {
@@ -15,10 +16,40 @@ interface QuestionDao : BaseDao<Question> {
         SELECT * FROM Question
         WHERE categoryId = :categoryId
         AND difficulty = :difficulty
+        AND id NOT IN (:excluded)
         ORDER BY RANDOM()
         LIMIT :amount
         """)
-    suspend fun getBy(categoryId: Int, difficulty: Difficulty, amount: Int): List<Question>
+    suspend fun getBy(categoryId: Int, difficulty: Difficulty, amount: Int, excluded: List<UUID> = emptyList()): List<Question>
+
+    @Query("""
+        SELECT * FROM Question
+        WHERE categoryId = :categoryId
+        ORDER BY RANDOM()
+        AND id NOT IN (:excluded)
+        LIMIT :amount
+        """)
+    suspend fun getBy(categoryId: Int, amount: Int, excluded: List<UUID> = emptyList()): List<Question>
+
+    @Query("""
+        SELECT * FROM Question
+        WHERE difficulty = :difficulty
+        AND id NOT IN (:excluded)
+        ORDER BY RANDOM()
+        LIMIT :amount
+        """)
+    suspend fun getBy(difficulty: Difficulty, amount: Int, excluded: List<UUID> = emptyList()): List<Question>
+
+    @Query("""
+        SELECT * FROM Question
+        WHERE id NOT IN (:excluded)
+        ORDER BY RANDOM()
+        LIMIT :amount
+        """)
+    suspend fun getBy(amount: Int, excluded: List<UUID> = emptyList()): List<Question>
+
+    @Query("SELECT EXISTS(SELECT * FROM Question WHERE id = :id AND question = :question)")
+    fun exists(id : UUID, question: String) : Boolean
 
     @Query("DELETE FROM Question")
     suspend fun deleteAll()
