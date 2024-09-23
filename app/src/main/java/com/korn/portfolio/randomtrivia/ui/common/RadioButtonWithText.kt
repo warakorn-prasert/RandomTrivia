1bef5b289d3c502c37764906922706d5f201fe97
Warakorn Mukdaprasert <warakorn.prasert@gmail.com>
2024-09-23 22:42:30 +0700

    feat: make RadioButtonWithText touch area circular

--- app/src/main/java/com/korn/portfolio/randomtrivia/ui/common/RadioButtonWithText.kt
+++ app/src/main/java/com/korn/portfolio/randomtrivia/ui/common/RadioButtonWithText.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -26,6 +28,7 @@ fun RadioButtonWithText(
 ) {
     Row(
         modifier = modifier
+            .clip(CircleShape)
             .clickable { onClick() }
             .heightIn(min = 40.dp)
             .padding(start = 12.dp, end = 16.dp),
