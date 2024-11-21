a649a532ecd30f37632e22a51d1e20de3b4321c5
Warakorn Mukdaprasert <warakorn.prasert@gmail.com>
2024-11-22 02:53:47 +0700

    refactor: use Modifier.matchParentSize() on scrim Canvas of custom bottom sheet

--- app/src/main/java/com/korn/portfolio/randomtrivia/ui/common/ScrimmableBottomSheetScaffold.kt
+++ app/src/main/java/com/korn/portfolio/randomtrivia/ui/common/ScrimmableBottomSheetScaffold.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,7 +25,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -34,7 +32,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
@@ -138,12 +135,6 @@ private fun BottomSheetScrimmableContent(
         scope.launch { sheetState.partialExpand() }
     }
 
-    val color = MaterialTheme.colorScheme.scrim.copy(alpha = 0.32f)
-    val alpha by animateFloatAsState(
-        targetValue = if (showScrim) 1f else 0f,
-        animationSpec = TweenSpec()
-    )
-
     val dismissSheet = if (showScrim) {
         Modifier
             .pointerInput(::onDismissRequest) {
@@ -155,23 +146,17 @@ private fun BottomSheetScrimmableContent(
     } else {
         Modifier
     }
-    var w by remember { mutableIntStateOf(0) }
-    var h by remember { mutableIntStateOf(0) }
-    val density = LocalDensity.current
-    Box(
-        Modifier.onSizeChanged {
-            w = it.width
-            h = it.height
-        }
-    ) {
+
+    Box {
+        val color = MaterialTheme.colorScheme.scrim.copy(alpha = 0.32f)
+        val alpha by animateFloatAsState(
+            targetValue = if (showScrim) 1f else 0f,
+            animationSpec = TweenSpec()
+        )
         content()
         Canvas(
             Modifier
-                // For implementation flexibility, fillMaxSize and intrinsic sizes are not used.
-                .size(
-                    width = with(density) { w.toDp() },
-                    height = with(density) { h.toDp() }
-                )
+                .matchParentSize()
                 .then(dismissSheet)
         ) {
             drawRect(color = color, alpha = alpha)
